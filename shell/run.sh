9c0af691c2a3a4f99b75a9f041d2ad0f6870d945
Vladislav Lukash <vlukash@sdvor.com>
2023-04-13 18:31:22 +0300

    add README

--- shell/run.sh
+++ shell/run.sh
@@ -1,2 +1,6 @@
+echo "deploy begin"
+
 docker-compose -f ./docker-compose.yaml -p greenbone-community-edition pull
 docker-compose -f ./docker-compose.yaml -p greenbone-community-edition up -d
+
+echo "deploy begin"
